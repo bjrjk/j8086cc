63da52baa221471220a86a9b14e56812d337a395
Jack Ren <bjrjk@qq.com>
2021-05-13 21:44:18 +0800

    Update Web Interface for new themes

--- web/gen.php
+++ web/gen.php
@@ -1,6 +1,7 @@
 <?php
 
-if(!isset($_POST['code']))die("No j8086c code content!");
+if(!isset($_POST['code']))
+	die("No j8086c code content!");
 
 $j8086BaseFileName = tempnam("/tmp","j8086");
 $j8086cName = $j8086BaseFileName . ".j8086c";
@@ -13,26 +14,86 @@ fclose($f);
 
 $command = "java -jar j8086cc.jar $j8086cName 2>&1";
 exec($command,$message,$retcode);
-if($retcode != 0)die("Error:\n". $message);
+if($retcode != 0){
+	var_dump($message);
+	die("\nCompilation failed due to the above error.\n");
+}
 
-echo '<p>j8086p (PreProcessed Code):</p>';
-echo '<textarea cols=40 rows=20>';
-echo file_get_contents($j8086pName);
-echo '</textarea>';
-
-echo '<p>j8086i (IR):</p>';
-echo '<textarea cols=40 rows=20>';
-echo file_get_contents($j8086iName);
-echo '</textarea>';
-
-echo '<p>j8086s (Intel 8086 ASM):</p>';
-echo '<textarea cols=40 rows=20>';
-echo file_get_contents($j8086sName);
-echo '</textarea>';
+$j8086c = file_get_contents($j8086cName);
+$j8086p = file_get_contents($j8086pName);
+$j8086i = file_get_contents($j8086iName);
+$j8086s = file_get_contents($j8086sName);
 
 unlink($j8086cName);
 unlink($j8086pName);
 unlink($j8086iName);
 unlink($j8086sName);
-
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.61.0/codemirror.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.61.0/codemirror.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.61.0/mode/clike/clike.min.js"></script>
+    <style>
+        .center {
+            text-align: center;
+        }
+    </style>
+    <title>Online j8086cc Compiler</title>
+</head>
+<body>
+<div class="container">
+    <div class="alert alert-primary center" role="alert">
+        Online j8086cc Compiler
+    </div>
+<p class="center">j8086c Code:</p>
+<textarea cols="40" rows="20" id="j8086c">
+<?php echo $j8086c; ?>
+</textarea>
+<br>
+<p class="center">j8086p (PreProcessed Code):</p>
+<textarea cols="40" rows="20" id="j8086p">
+<?php echo $j8086p; ?>
+</textarea>
+<br>
+<p class="center">j8086i (IR):</p>
+<textarea cols="40" rows="20" id="j8086i">
+<?php echo $j8086i; ?>
+</textarea>
+<br>
+<p class="center">j8086s (Intel 8086 ASM):</p>
+<textarea cols="40" rows="20" id="j8086s">
+<?php echo $j8086s; ?>
+</textarea>
+
+<div class="center">
+<p>Powered By:
+<a href="https://github.com/bjrjk/j8086cc/">bjrjk/j8086cc</a>
+</p>
+<br>
+</div>
+</div>
+<script>
+var j8086c = CodeMirror.fromTextArea(document.getElementById("j8086c"), {
+    mode: "text/x-csrc",
+    lineNumbers: true,
+});
+var j8086p = CodeMirror.fromTextArea(document.getElementById("j8086p"), {
+    mode: "text/x-csrc",
+    lineNumbers: true,
+});
+var j8086i = CodeMirror.fromTextArea(document.getElementById("j8086i"), {
+    lineNumbers: true,
+});
+var j8086s = CodeMirror.fromTextArea(document.getElementById("j8086s"), {
+    lineNumbers: true,
+});
+</script>
+</body>
+</html>
